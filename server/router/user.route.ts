715f2e8bb245fe0e1cc33e28ea71a219391c6269
trngnneee <dtn06052005@gmail.com>
2025-06-02 19:12:54 +0700

    Thêm validate bên BE, xử lý chuyển hướng khi đăng ký xong bên FE

--- server/router/user.route.ts
+++ server/router/user.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import * as userController from "../controller/user.controller";
+import * as userValidate from "./../validate/user.validate"
 
 const router = Router();
 
 router.post(
   '/register', 
+  userValidate.registerPost,
   userController.registerPost
 )
 
